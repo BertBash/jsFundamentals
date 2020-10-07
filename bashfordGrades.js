351b15326c9e309eec450e6ec2dd02246b6209c7
Travis Davis <52011418+TravisDavis-ops@users.noreply.github.com>
2020-10-07 06:38:48 -0500

    Type Casting
    Type cast are often unessery in javascript due to its flexable type system.

--- bashfordGrades.js
+++ bashfordGrades.js
@@ -1,4 +1,4 @@
 var maxPoints = 50;
 var points;
-points = Number(prompt('How many points did you get?'));
-alert(String(points/50*100)+'%');
+points = prompt('How many points did you get?');
+alert((points/50*100)+'%');
